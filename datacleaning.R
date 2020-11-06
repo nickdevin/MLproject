6a1bc135ab09dd635e3b4a8849836fe36c5bbf35
nickdevin <70073666+nickdevin@users.noreply.github.com>
2020-11-05 16:27:35 -0800

    write csv added

--- datacleaning.R
+++ datacleaning.R
@@ -2,7 +2,7 @@ library(dplyr)
 
 homes = read.csv('train.csv')
 
-
+# testing data cleaning procedure on original train dataset
 # remove columns that will not be used in analysis
 remove.cols = function(DF) {
   DF = DF %>% dplyr::select(., -Id,
@@ -11,7 +11,8 @@ remove.cols = function(DF) {
                             -PoolArea,
                             -MiscFeature,
                             -GarageYrBlt,
-                            -Fence)
+                            -Fence,
+                            -'Unnamed: 0')
   return(DF)
 }
 
@@ -36,16 +37,16 @@ homes3 = impute.nones(homes2)
 stillmissing = colSums(is.na(homes3))
 stillmissing[stillmissing > 0] # Columns still containing missing values
 
-impute.electrical = function(DF) {
-  DF$Electrical[is.na(DF$Electrical)] =
-    names(sort(table(DF$Electrical), decreasing = TRUE))[1]
+impute.with.mode = function(DF, feature) {
+  DF$feature[is.na(DF$feature)] =
+    names(sort(table(DF$feature), decreasing = TRUE))[1]
   return(DF)
 }
 
 homes4 = impute.electrical(homes3)
 
 stillmissing = colSums(is.na(homes4))
-stillmissing[stillmissing > 0] # Columns still containing missing values
+names(stillmissing[stillmissing > 0]) # Columns still containing missing values
                                # Use KNN imputation with k = 9 for remaining NAs
                                # Assumption: Houses of similar design will have
                                # similar values for the remaining columns.
@@ -66,3 +67,29 @@ drops = c('LotFrontage_imp', 'MasVnrType_imp', 'MasVnrArea_imp')
 homes.imp = homes.knn[, !names(homes.knn) %in% drops]
 
 sum(colSums(is.na(homes.imp))) #no missing values remain
+
+write.csv(homes.imp, 'homes_final.csv')
+
+
+# a more general function for cleaning data
+# missingness in test dataset is different from train
+
+cleandata = function(DF) {
+  DF = remove.cols(DF)
+  DF = impute.nones(DF)
+  DF = impute.with.mode(DF, Electrical)
+  DF = impute.with.mode(DF, Utilities)
+  DF = impute.with.mode(DF, Functional)
+  
+  stillmissing = colSums(is.na(DF))
+  to.impute = names(stillmissing[stillmissing > 0])
+  
+  to.drop = paste0(to.impute, '_imp')
+  
+  DF.knn = kNN(DF, to.impute, k=9)
+  DF.imp = DF.knn[, !names(DF.knn) %in% to.drop]
+  return(DF.imp)
+}
+
+test.cleaned = cleandata(read.csv('test.csv'))
+write.csv(test.cleaned, 'test_final.csv')
